a617cad7316ba60ef0dc9700662f9549cf01543a
Skillz <gankstarsleague@gmail.com>
2021-02-24 21:08:57 -0500

    chore: use latest

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
-export * from "https://deno.land/x/discordeno@10.2.0/mod.ts";
+export * from "https://deno.land/x/discordeno@10.3.0/mod.ts";
+export * from "https://deno.land/std@0.88.0/http/server.ts";

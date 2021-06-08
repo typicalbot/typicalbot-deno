fa85ecb5750f3d216affed4f1b461b5a0e81a43f
Nicholas Sylke <nicksylke@outlook.com>
2021-06-07 21:44:03 -0500

    refactor: add ms to ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,7 @@
 import Command from "../common/command/Command.ts";
 
 const PingCommand: Command = (message) => {
-  return message.send("Pong!");
+  return message.send(`Pong! MS: ${Date.now() - message.timestamp}ms`);
 };
 
 export default PingCommand;

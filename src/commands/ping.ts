7de1ecb73b9a00a97f08b5c29f9dab1b0f2e281f
Nicholas Sylke <nicksylke@outlook.com>
2021-06-11 17:18:41 -0500

    refactor: command execution from editting the original response

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,10 @@
 import Command from "../common/command/Command.ts";
 import { bot } from "../cache.ts";
 
-const PingCommand: Command = (message) => {
-  return message.send(`Pong! MS: ${Date.now() - message.timestamp}ms`);
+const PingCommand: Command = async (message) => {
+  const ping = await message.send(`Pong!`);
+
+  return ping.edit(`Pong! Command Execution: ${ping.timestamp - message.timestamp}ms`);
 };
 
 bot.commands.set("ping", PingCommand);

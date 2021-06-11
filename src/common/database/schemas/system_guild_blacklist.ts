5a3628440091db3aa368e69f83a6ce0adcc7414e
Nicholas Sylke <nicksylke@outlook.com>
2021-06-10 19:08:43 -0500

    refactor!: remove database structures

--- src/common/database/schemas/system_guild_blacklist.ts
+++
@@ -1,9 +0,0 @@
-interface SystemGuildBlacklist {
-  _id: { $oid: string };
-  guildId: string;
-  reason: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-export default SystemGuildBlacklist;
--- src/common/database/schemas/system_user_blacklist.ts
+++
@@ -1,9 +0,0 @@
-interface SystemUserBlacklist {
-  _id: { $oid: string };
-  userId: string;
-  reason: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-export default SystemUserBlacklist;

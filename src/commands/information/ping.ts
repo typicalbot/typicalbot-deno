cc37650569da372584a4b2eff006cc591ab21f82
Nicholas Sylke <nicksylke@outlook.com>
2021-06-11 19:38:10 -0500

    chore: deno fmt

--- src/commands/information/ping.ts
+++ src/commands/information/ping.ts
@@ -4,7 +4,9 @@ import { bot } from "../../cache.ts";
 const PingCommand: Command = async (message) => {
   const ping = await message.send(`Pong!`);
 
-  return ping.edit(`Pong! Command Execution: ${ping.timestamp - message.timestamp}ms`);
+  return ping.edit(
+    `Pong! Command Execution: ${ping.timestamp - message.timestamp}ms`,
+  );
 };
 
 bot.commands.set("ping", PingCommand);

d3e1b9b7e951b72a5fbbaae651d4c4b93c7548c9
Nicholas Sylke <nicksylke@outlook.com>
2021-06-12 22:11:59 -0500

    feat: duck command

---
+++ src/commands/animal/duck.ts
@@ -0,0 +1,11 @@
+import Command from "../../common/command/Command.ts";
+import { bot } from "../../cache.ts";
+
+const DuckCommand: Command = (message) => {
+  fetch("https://random-d.uk/api/v2/quack")
+    .then((res) => res.json())
+    .then((json) => message.send(json.url))
+    .catch(() => message.send("Unable to query from API."));
+};
+
+bot.commands.set("duck", DuckCommand);

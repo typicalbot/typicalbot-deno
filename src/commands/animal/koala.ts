4225c5c0622ebe3824e49af7e91f9b9e82b22877
Nicholas Sylke <nicksylke@outlook.com>
2021-06-12 22:23:44 -0500

    feat: koala command

---
+++ src/commands/animal/koala.ts
@@ -0,0 +1,11 @@
+import Command from "../../common/command/Command.ts";
+import { bot } from "../../cache.ts";
+
+const KoalaCommand: Command = (message) => {
+  fetch("https://some-random-api.ml/img/koala")
+    .then((res) => res.json())
+    .then((json) => message.send(json.link))
+    .catch(() => message.send("Unable to query from API."));
+};
+
+bot.commands.set("koala", KoalaCommand);

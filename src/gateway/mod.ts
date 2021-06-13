534605edebec808c87698c2c2699abbee1523a0d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-13 22:24:10 +0000

    fix: token for rest for resharding

--- src/gateway/mod.ts
+++ src/gateway/mod.ts
@@ -1,9 +1,12 @@
-import { startGateway } from "../../deps.ts";
+import { rest, startGateway } from "../../deps.ts";
 import {
   DISCORD_TOKEN,
   EVENT_HANDLER_SECRET_KEY,
   EVENT_HANDLER_URL,
 } from "../../config.ts";
+
+// Prepare the `rest` system to work. FOR RESHARDER needs to getGatewayBot
+rest.token = `Bot ${DISCORD_TOKEN}`;
 
 export function startup() {
   startGateway({

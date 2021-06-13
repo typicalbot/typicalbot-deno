f54f1c9fef964f3c7a117a2fb17726c826e1ab90
Nicholas Sylke <nicksylke@outlook.com>
2021-06-13 16:47:34 -0500

    refactor: remove ready event - not emitted via proxy

--- src/events/ready.ts
+++
@@ -1,8 +0,0 @@
-import { bot } from "../cache.ts";
-
-const Ready = () => {
-  console.log(`Loaded ${bot.commands.size} commands`);
-  console.log(`Loaded ${Object.keys(bot.events).length} events`);
-};
-
-bot.events.ready = Ready;

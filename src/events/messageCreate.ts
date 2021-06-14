c8cafcfcb32085f406b5d3634deb46c14263e4fa
Nicholas Sylke <nicksylke@outlook.com>
2021-06-14 18:49:26 -0500

    refactor: return prefix is mentioned with no arguments

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,7 @@
-import { DiscordChannelTypes, DiscordenoMessage } from "../../deps.ts";
+import { botId, DiscordChannelTypes, DiscordenoMessage } from "../../deps.ts";
 import { bot } from "../cache.ts";
+
+const prefix = "$$";
 
 const MessageCreate = (message: DiscordenoMessage) => {
   // Ignore bots
@@ -11,11 +13,17 @@ const MessageCreate = (message: DiscordenoMessage) => {
   // Get raw command and arguments
   const [raw, ...args] = message.content.split(" ");
 
+  // Check for mention and no arguments
+  if ((raw === `<@!${botId}>` || raw === `<@${botId}>`) && !args.length) {
+    // Send prefix for server
+    return message.send(`The prefix for this server is \`${prefix}\``);
+  }
+
   // TODO: Use prefix from guild settings
-  if (!raw.startsWith("$$")) return;
+  if (!raw.startsWith(prefix)) return;
 
   // Get command name without prefix
-  const commandName = raw.slice("$$".length).toLowerCase();
+  const commandName = raw.slice(prefix.length).toLowerCase();
   const command = bot.commands.get(commandName);
 
   // Command doesn't exist

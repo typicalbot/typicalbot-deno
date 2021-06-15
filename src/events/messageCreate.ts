c4bde60c82d88ab2910cf9d0647f0fe1d3e5b714
Nicholas Sylke <nicksylke@outlook.com>
2021-06-14 19:50:24 -0500

    feat: permission check for commands

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,9 +1,17 @@
-import { botId, DiscordChannelTypes, DiscordenoMessage } from "../../deps.ts";
+import {
+  botId,
+  DiscordChannelTypes,
+  DiscordenoMessage,
+  getMissingChannelPermissions,
+  getMissingGuildPermissions,
+  Permission,
+  PermissionStrings,
+} from "../../deps.ts";
 import { bot } from "../cache.ts";
 
 const prefix = "?";
 
-const MessageCreate = (message: DiscordenoMessage) => {
+const MessageCreate = async (message: DiscordenoMessage) => {
   // Ignore bots
   if (message.isBot) return;
 
@@ -29,10 +37,91 @@ const MessageCreate = (message: DiscordenoMessage) => {
   // Command doesn't exist
   if (!command) return;
 
+  // Check for bot permissions
+  if (command.options?.permissions?.self?.length) {
+    if (
+      await checkPermissions(
+        message,
+        "self",
+        command.options?.permissions?.self,
+      )
+    ) {
+      return;
+    }
+  }
+
+  // Check for user permissions
+  if (command.options?.permissions?.user?.length) {
+    if (
+      await checkPermissions(
+        message,
+        "user",
+        command.options?.permissions?.user,
+      )
+    ) {
+      return;
+    }
+  }
+
   // Grab default arguments
   const finalArgs = (args.length ? args : command.defaultArguments || []);
 
   return command(message, finalArgs);
+};
+
+// TODO: Create interface in Command file for type?
+//  Move this to a separate file?
+const checkPermissions = async (
+  message: DiscordenoMessage,
+  type: "user" | "self",
+  permissions: Permission[],
+): Promise<boolean> => {
+  const id = type === "self" ? botId : message.authorId;
+
+  const channelPermissionsMissing = await getMissingChannelPermissions(
+    message.channelId,
+    id,
+    permissions,
+  );
+
+  if (!channelPermissionsMissing.length) {
+    return false;
+  }
+
+  const guildPermissionsMissing = await getMissingGuildPermissions(
+    message.guildId,
+    id,
+    permissions,
+  );
+
+  if (!guildPermissionsMissing.length) {
+    return false;
+  }
+
+  const sendViewPermissions = (permissions: PermissionStrings[]): boolean =>
+    permissions.some((permission) =>
+      permission === "SEND_MESSAGES" || permission === "VIEW_CHANNEL"
+    );
+
+  if (channelPermissionsMissing.length) {
+    if (sendViewPermissions(channelPermissionsMissing)) return true;
+
+    message.send(
+      `<@!${id}> is missing the following permission(s): ${
+        channelPermissionsMissing.join(", ")
+      }`,
+    );
+    return true;
+  } else {
+    if (sendViewPermissions(guildPermissionsMissing)) return true;
+
+    message.send(
+      `<@!${id}> is missing the following permission(s): ${
+        guildPermissionsMissing.join(", ")
+      }`,
+    );
+    return true;
+  }
 };
 
 bot.events.messageCreate = MessageCreate;

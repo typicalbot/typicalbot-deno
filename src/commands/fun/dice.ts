c27621b90e39ab90d3716ef3dca752469653b2c8
Nicholas Sylke <nicksylke@outlook.com>
2021-06-16 16:14:23 -0500

    chore: deno fmt

--- src/commands/fun/dice.ts
+++ src/commands/fun/dice.ts
@@ -16,53 +16,53 @@ const DiceCommand: Command = (interaction) => {
   }
 
   if (raw.type === DiscordApplicationCommandOptionTypes.String && raw.value) {
-      const [dices, sides] = raw.value.split('d');
+    const [dices, sides] = raw.value.split("d");
 
-      if (!dices || !sides) {
-        return basicInteractionResponse(
-            interaction.id,
-            interaction.token,
-            "Invalid usage, use `2d6` as an example. Where `2` is the amount of dices and `6` is amount of sides.",
-          );
-      }
-
-      const convertedDices = +dices;
-      const convertedSides = +sides;
-      
-      if (convertedDices < 2) {
-        return basicInteractionResponse(
-            interaction.id,
-            interaction.token,
-            "There must be at least two dices.",
-          );
-      }
-
-      if (convertedSides < 2) {
-        return basicInteractionResponse(
-            interaction.id,
-            interaction.token,
-            "There must be at least two sides.",
-          );
-      } 
-
-      const rolls = [];
-
-      const rollDice = (sides: number): number => {
-        sides = Math.max(sides, 1);
-        return Math.floor(Math.random() * (sides - 1 + 1)) + 1;
-      };
-
-      for (let i = 0; i < convertedDices; i++) {
-        rolls.push(rollDice(convertedSides));
-      }
-
-      const sum = rolls.reduce((a, b) => a + b, 0);
-
+    if (!dices || !sides) {
       return basicInteractionResponse(
         interaction.id,
         interaction.token,
-        `You rolled [${rolls.join(', ')}] totaling ${sum}.`,
+        "Invalid usage, use `2d6` as an example. Where `2` is the amount of dices and `6` is amount of sides.",
       );
+    }
+
+    const convertedDices = +dices;
+    const convertedSides = +sides;
+
+    if (convertedDices < 2) {
+      return basicInteractionResponse(
+        interaction.id,
+        interaction.token,
+        "There must be at least two dices.",
+      );
+    }
+
+    if (convertedSides < 2) {
+      return basicInteractionResponse(
+        interaction.id,
+        interaction.token,
+        "There must be at least two sides.",
+      );
+    }
+
+    const rolls = [];
+
+    const rollDice = (sides: number): number => {
+      sides = Math.max(sides, 1);
+      return Math.floor(Math.random() * (sides - 1 + 1)) + 1;
+    };
+
+    for (let i = 0; i < convertedDices; i++) {
+      rolls.push(rollDice(convertedSides));
+    }
+
+    const sum = rolls.reduce((a, b) => a + b, 0);
+
+    return basicInteractionResponse(
+      interaction.id,
+      interaction.token,
+      `You rolled [${rolls.join(", ")}] totaling ${sum}.`,
+    );
   }
 };
 

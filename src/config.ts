fbdbf301b55ac3d33ccd27d78e6aced8220e1f7e
Tyler Richards <hi@tjr.gg>
2021-06-18 22:20:52 -0500

    feat: add dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import "https://deno.land/x/dotenv/load.ts";
+
 export const DISCORD_TOKEN = Deno.env.get("DISCORD_TOKEN");
 export const DISCORD_ID = Deno.env.get("DISCORD_ID");
 export const EVENT_HANDLER_PORT = Deno.env.get("EVENT_HANDLER_PORT");

dde605e1f5795690d7edcc544aa569a093dd6998
Nicholas Sylke <nicksylke@outlook.com>
2021-06-18 21:17:58 -0500

    feat: docs command

---
+++ src/commands/general/docs.ts
@@ -0,0 +1,19 @@
+import Command, {
+  basicInteractionResponse,
+} from "../../common/command/Command.ts";
+import { bot } from "../../cache.ts";
+
+const DocsCommand: Command = (interaction) => {
+  return basicInteractionResponse(
+    interaction.id,
+    interaction.token,
+    "You can find our documentation at https://typicalbot.com/docs.",
+  );
+};
+
+DocsCommand.options = {
+  name: "docs",
+  description: "No description available.",
+};
+
+bot.commands.set("docs", DocsCommand);

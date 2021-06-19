1892e130890b8c48cb8ac4e08b0d3a96d927d331
Nicholas Sylke <nicksylke@outlook.com>
2021-06-18 21:18:21 -0500

    feat: premium command

---
+++ src/commands/general/premium.ts
@@ -0,0 +1,19 @@
+import Command, {
+  basicInteractionResponse,
+} from "../../common/command/Command.ts";
+import { bot } from "../../cache.ts";
+
+const PremiumCommand: Command = (interaction) => {
+  return basicInteractionResponse(
+    interaction.id,
+    interaction.token,
+    "You can learn more about TypicalBot Premium at https://typicalbot.com/pricing.",
+  );
+};
+
+PremiumCommand.options = {
+  name: "premium",
+  description: "No description available.",
+};
+
+bot.commands.set("premium", PremiumCommand);
